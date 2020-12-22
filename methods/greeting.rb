c8c68a32e945b396ad41b0584379f3aef8f58c40
Andrew Moore <andrewdmoore84@gmail.com>
2020-12-21 19:41:01 -0500

    Add exercises for Methods chapter

---
+++ methods/greeting.rb
@@ -0,0 +1,7 @@
+def greeting (name)
+  "How's it going, #{name}?"
+end
+
+name = "Phi"
+
+p greeting(name)
---
+++ methods/multiply.rb
@@ -0,0 +1,6 @@
+def multiply (x, y)
+  x * y
+end
+
+p "multiply(3, 5) = " + multiply(3, 5).to_s
+p "multiply(8, 4) = " + multiply(8, 4).to_s 
